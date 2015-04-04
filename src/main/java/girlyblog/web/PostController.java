d17c5d81acc8847194632926d9546ec08902e74b
Hanneke Debie <girlysprite@gmail.com>
2015-04-04 14:20:32 +0200

    more comments

--- src/main/java/girlyblog/web/PostController.java
+++ src/main/java/girlyblog/web/PostController.java
@@ -1,6 +1,15 @@
-package girlyblog.web;
+/**
+ * This is a controller class.
+ * <p>The Controller annotation marks it as such. This class gets the webrequests through the RequestMapping annotation.
+ * Based on which part of the webspage is called (the stuff behind the slash) it directs the rest of the code to do different things.</p>
+ * <p>A logger is also included.</p>
+ * @author Hanneke Debie
+ * @version 0.1
+ */
 
-import girlyblog.domain.Post;
+package girlyblog.web; //Dit geeft tot welke package deze file behoort.
+
+import girlyblog.domain.Post; //post included (mn eigen code) om er gebruik van te kunnen maken.
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -16,7 +25,9 @@ public class PostController {
 
     @RequestMapping("/post")
     public String post(Model model) {  // een model is een manier om informatie naar de template te krijgen.
-        Post post = new Post();
+        Post post = new Post(); //Hier maak ik een post object, die alle variabelen in zich heeft die ik nodig heb.
+        //zo kan ik een hele lading variabelen naar het template toe krijgen.
+        //Hier kan ik straks eerst data uit een DB halen en dan toewijzen ipv hier zelf strings verzinnen.
         post.setTitle("This is a post.");
         post.setBody("This is a short post.");
         post.setPublishDate(LocalDateTime.now());
