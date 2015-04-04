a4a4d85039fb205df71912ac4622276cb50fcf8c
Hanneke Debie <girlysprite@gmail.com>
2015-04-04 14:20:43 +0200

    more comments

--- src/main/java/girlyblog/GirlyBlogApplication.java
+++ src/main/java/girlyblog/GirlyBlogApplication.java
@@ -3,10 +3,11 @@ package girlyblog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication  //Dit markeert de class als een application class
 public class GirlyBlogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GirlyBlogApplication.class, args);
+        SpringApplication.run(GirlyBlogApplication.class, args); //hoe werkt dit precies? Waarom is het zo opgezet?
+        //wel duidelijk dat dit deel is van de initializatie.
     }
 }
