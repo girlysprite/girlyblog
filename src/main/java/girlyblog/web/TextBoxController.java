9256e0dd21a2a43ee71d233e84de07e51ab97b56
Hanneke Debie <girlysprite@gmail.com>
2015-05-18 21:27:45 +0200

    hij werkt!

--- src/main/java/girlyblog/web/TextBoxController.java
+++ src/main/java/girlyblog/web/TextBoxController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller  //Dit markeert de java class als een controller class.
